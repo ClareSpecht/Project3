b9d59bc79c73c725fe6ef6eb4ffaa6918f21a2fb
PielordZZ <zzpieme@gmail.com>
2022-04-07 19:13:28 -0400

    imported dependancies and setup a test for calls

--- Data Collection/data_init.py
+++ Data Collection/data_init.py
@@ -6,16 +6,20 @@ import pandas as pd
 
 engine = create_engine("sqlite:///happiness_comparisons.sqlite")
 
-pet_table = pd.read_json('Raw Data Files/pet_data.json')
 
-pet_table.to_sql('pets', engine)
+#create dataframe tables to insert
+pet_table = pd.read_json('Raw Data Files/pet_data.json')
 
 income_table = pd.read_csv('Raw Data Files/IncomeData.csv')
 
-
 states_table = pd.read_csv('Raw Data Files/states.csv')
 
+
+
+
+#add tables to database
 states_table.to_sql('states',engine)
 
 income_table.to_sql('income',engine)
 
+pet_table.to_sql('pets', engine)
