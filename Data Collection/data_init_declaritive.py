75a3be6b5c1e74c5013a624211a77273ad694145
PielordZZ <zzpieme@gmail.com>
2022-04-09 11:52:02 -0400

    added all tables to sqlite db

--- Data Collection/data_init_declaritive.py
+++ Data Collection/data_init_declaritive.py
@@ -23,7 +23,6 @@ statepark_table = pd.read_csv('Raw Data Files/stateparks.csv')
 
 crime_table = pd.read_csv('Raw Data Files/crime_data.csv')
 
-
 with engine.connect() as con:
     con.execute('''create TABLE pets("index" INTEGER, name VARCHAR PRIMARY KEY,  abbreviation VARCHAR(2),"pet ownership" VARCHAR,"dog ownership" VARCHAR,"cat ownership" VARCHAR);''')
 
@@ -34,16 +33,16 @@ with engine.connect() as con:
     con.execute('''create TABLE states('index' INTEGER, "State" VARCHAR PRIMARY KEY,  "State.1" VARCHAR,"State Capital" VARCHAR,Region VARCHAR);''')
 
 with engine.connect() as con:
-    con.execute('''CREATE TABLE all_parks("index" INTEGER, State VARCHAR,0 VARCHAR,1 VARCHAR,2 VARCHAR,3 VARCHAR,4 VARCHAR,5 VARCHAR,6 VARCHAR,7 VARCHAR,8 VARCHAR,9 VARCHAR,10 VARCHAR,11 VARCHAR,12 VARCHAR,13 VARCHAR,14 VARCHAR,15 VARCHAR,16 VARCHAR,17 VARCHAR,18 VARCHAR,StateAbv VARCHAR(2),State Capital VARCHAR,Region VARCHAR);''')
+    con.execute('''CREATE TABLE all_parks("index" INTEGER, State VARCHAR,'0' VARCHAR,'1' VARCHAR,'2' VARCHAR,'3' VARCHAR,'4' VARCHAR,'5' VARCHAR,'6' VARCHAR,'7' VARCHAR,'8' VARCHAR,'9' VARCHAR,'10' VARCHAR,'11' VARCHAR,'12' VARCHAR,'13' VARCHAR,'14' VARCHAR,'15' VARCHAR,'16' VARCHAR,'17' VARCHAR,'18' VARCHAR,StateAbv VARCHAR(2) PRIMARY KEY,'State Capital' VARCHAR,Region VARCHAR);''')
 
 with engine.connect() as con:
-    con.execute('''create TABLE state_parks('index' INTEGER, Count INTEGER,Type VARCHAR,State VARCHAR,State.1 VARCHAR(2),State Capital VARCHAR,Region VARCHAR);''')
+    con.execute('''create TABLE state_parks('index' INTEGER, Count INTEGER,Type VARCHAR,State VARCHAR PRIMARY KEY,'State.1' VARCHAR(2),'State Capital' VARCHAR,Region VARCHAR);''')
 
 with engine.connect() as con:
-    con.execute('''create TABLE crime('index' INTEGER, "State" VARCHAR PRIMARY KEY,  "State.1" VARCHAR,"State Capital" VARCHAR,Region VARCHAR,Crime INTEGER);''')
+    con.execute('''create TABLE crime('index' INTEGER, "State" VARCHAR PRIMARY KEY, state_abv VARCHAR(2),  "State Capital" VARCHAR,Region VARCHAR,Crime INTEGER);''')
 
 with engine.connect() as con:
-    con.execute('''create TABLE colleges('index' INTEGER, "StateABV" VARCHAR PRIMARY KEY,  University INTEGER);''')
+    con.execute('''create TABLE colleges('index' INTEGER, "StateABV" VARCHAR PRIMARY KEY,  Unviersity INTEGER, State VARCHAR, 'State Capital' VARCHAR, Region VARCHAR);''')
 
 
 
@@ -54,10 +53,10 @@ income_table.to_sql('income',engine, if_exists='append')
 
 pet_table.to_sql('pets', engine, if_exists='append')
 
-park_table.to_sql('parks', engine, if_exists='append')
-
-college_table.to_sql('colleges', engine, if_exists='append')
+park_table.to_sql('all_parks', engine, if_exists='append')
 
 statepark_table.to_sql('state_parks', engine, if_exists='append')
 
 crime_table.to_sql('crime', engine, if_exists='append')
+
+college_table.to_sql('colleges', engine, if_exists='append')
